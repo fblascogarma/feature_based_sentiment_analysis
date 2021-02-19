955a211079df25f3342dfdf41e8262ef237e9f09
Francisco Blasco <62343770+fblascogarma@users.noreply.github.com>
2021-02-19 09:35:50 -0500

    Eliminated comments to clean the code

--- absa.py
+++ absa.py
@@ -32,8 +32,6 @@ df_reelgood = pd.DataFrame({
 
 # 2. Convert the text to lowercase and remove punctuation and white space
 df_reelgood['remove_lower_punct'] = df_reelgood['reviews'].str.lower().str.replace("'", '', regex=True).str.replace('[^\w\s]', ' ', regex=True).str.replace(" \d+", " ", regex=True).str.replace(' +', ' ', regex=True).str.strip()
-#print(df_reelgood.head())
-#print(len(df_reelgood))
 
 # 3. Apply sentiment analysis using VADER
 analyser = SentimentIntensityAnalyzer()                         # this is a rule-based sentiment analysis tool for social media
@@ -56,22 +54,18 @@ for i in df_reelgood['remove_lower_punct'].values.tolist():
     
 df_reelgood['sentiment'] = sentiment_label_list
 df_reelgood['sentiment score'] = sentiment_score_list
-#print(df_reelgood.head())
 
 # 4. Tokenise string
 df_reelgood['tokenise'] = df_reelgood.apply(lambda row: nltk.word_tokenize(row[1]), axis=1)         # <apply> function to tokenize and <lambda> to find length of each text
-#print(df_reelgood.head())
 
 # 5. Initiate stopwords from nltk, add additional missing terms, and remove stopwords
 stop_words = stopwords.words(language_of_reviews)                       # there are 21 languages to use and you can check the options going to C:/Users/username/AppData/Roming/nltk_data/corpora/stopwords
 stop_words.extend(stop_words_list)                                      # to increase the stop_words list with the words we included in our file with that name
 df_reelgood['remove_stopwords'] = df_reelgood['tokenise'].apply(lambda x: [item for item in x if item not in stop_words])
-#print("step 5: ", df_reelgood.head())
 
 # 6. Initiate nltk Lemmatiser and Lemmatise words
 wordnet_lemmatizer = WordNetLemmatizer()
 df_reelgood['lemmatise'] = df_reelgood['remove_stopwords'].apply(lambda x: [wordnet_lemmatizer.lemmatize(y) for y in x])
-#print(df_reelgood.head())
 
 # 7. Initialize the count vectorizer and join the processed data to be a vectorised
 vectorizer = CountVectorizer(analyzer = 'word', ngram_range = (2, 2))       # convert a collection of text documents to a matrix of token counts
@@ -79,7 +73,6 @@ vectors = []
 for index, row in df_reelgood.iterrows():
     vectors.append(", ".join(row[6]))
 vectorised = vectorizer.fit_transform(vectors)
-#print(vectorised)
 
 # 8. Initisalize LDA model and make the df
 lda_model = LatentDirichletAllocation(n_components = aspects_no,            # number of topics; default = 10
@@ -93,42 +86,33 @@ lda_output = lda_model.fit_transform(vectorised)
 topic_names = ["Topic" + str(i) for i in range(1, lda_model.n_components + 1)]      # col names
 
 df_reelgood_document_topic = pd.DataFrame(np.round(lda_output, 2), columns = topic_names)
-#print("df_reelgood_document_topic: ", df_reelgood_document_topic)
 
 # 9. Get dominant topic for each document and join to original df
 dominant_topic = (np.argmax(df_reelgood_document_topic.values, axis=1)+1)
-df_reelgood_document_topic['Dominant_topic'] = dominant_topic
-# print("Step 9 doc topic: ", df_reelgood_document_topic)
-# print("Step 9 df pre-merge: ", df_reelgood.reset_index())
+df_reelgood_document_topic['Dominant_topic'] = dominant_topi
 
 df_reelgood = df_reelgood.reset_index()             # reset index to do a proper merge between the 2 df
 df_reelgood = pd.merge(df_reelgood, df_reelgood_document_topic, left_index = True, right_index = True, how = 'outer')
-# print("Step 9 df: ", df_reelgood)
 
 # 10. Keywords the LDA extracted from such reviews
 docnames = ['Doc' + str(i) for i in range(len(df_reviews[df_reviews['App_Name']==app_name]))]                     # index names (one doc per row)
 df_reelgood_document_topic = pd.DataFrame(np.round(lda_output, 2), columns=topic_names, index=docnames)             # make the df of docs
-#print(df_reelgood_document_topic)
 
 
 dominant_topic = np.argmax(df_reelgood_document_topic.values, axis=1)           # get dominant topic position for each document (0 is Topic1, 1 is Topic2, and so on)
 df_reelgood_document_topic['dominant_topic'] = dominant_topic
-#print(df_reelgood_document_topic)
 
 df_reelgood_topic_keywords = pd.DataFrame(lda_model.components_)                # topic-keyword Matrix
-#print(df_reelgood_topic_keywords)
 
 
 df_reelgood_topic_keywords.columns = vectorizer.get_feature_names()             # assign Column and Index
 df_reelgood_topic_keywords.index = topic_names
 
 df_topic_no = pd.DataFrame(df_reelgood_topic_keywords.idxmax())
-#print(df_topic_no)
 df_scores = pd.DataFrame(df_reelgood_topic_keywords.max())
 
 tmp = pd.merge(df_topic_no, df_scores, left_index=True, right_index=True)
 tmp.columns = ['topic', 'relevance_score']
-#print(tmp)
 
 # 11. Determine which aspect a keyword belongs to, order df in descending order, and select the one with the highest score
 all_topics = []
@@ -145,13 +129,9 @@ for i in tmp['topic'].unique():
     all_topics.append(tmp_2)
 
 all_topics = pd.DataFrame(all_topics, columns=['Dominant_topic', 'topic_name'])
-#print("all_topics: ", all_topics)
 
 # 12. Results
-#print("df_reelgood: ", df_reelgood)
-#df_reelgood.to_csv("absa_reelgood.csv", encoding='utf8', index=False)
 results = df_reelgood.groupby(['Dominant_topic', 'sentiment']).count().reset_index()
-#print("results: ", results)
 
 # 13. Export data to an Excel file
 
